7d543213afcf0499d9ae941229244a0beafdfaa1
ddjr <ddalyjr@gmail.com>
2022-04-03 01:56:02 -0400

    ADD: added visualiztion project

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,15 +1,21 @@
-import React from 'react'
-import './About.scss'
-import Face from '../../media/face.jpg'
+import React from "react";
+import "./About.scss";
+import Face from "../../media/face.jpg";
 function About() {
   return (
-    <div className='about' id='about'>
+    <div className="about" id="about">
       <h1>About</h1>
       <img src={Face} />
-      <p> Hi there! David is a Software Engineer from the United States. He starting college at 14 years old, which is where he learned to code. He recieved his degree in Computer Science from Kalamazoo College and has been coding professionally since 2014. David has worked on enterprise software used by Fortune 10 companies, As well as on startup projects.
+      <p>
+        {" "}
+        Hi there! David is a Software Engineer from the United States. He
+        starting college at 14 years old, where he learned to code. He recieved
+        his degree in Computer Science from Kalamazoo College and has been
+        coding professionally since 2014. David has worked on enterprise
+        software used by Fortune 10 companies, as well as startup projects.
       </p>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
--- src/data.js
+++ src/data.js
@@ -1,34 +1,47 @@
-import Zelda from './media/zelda.gif'
-import Amazon from './media/amazon.gif'
-import Disney from './media/disney.gif'
-import Arcade from './media/arcade.gif'
+import Zelda from "./media/zelda.gif";
+import Amazon from "./media/amazon.gif";
+import Disney from "./media/disney.gif";
+import Arcade from "./media/arcade.gif";
+import Visualizer from "./media/visualizer.gif";
 export const Projects = [
-    {
-        id: 1,
-        title: "Adventure Game",
-        description: "Use your arrow keys to find the grail in every level. Player learns to swim and unlock doors. Built with OOP & pure JS. Uses a Canvas element and clock to render levels. Dynamic camera scrolls as player moves.",
-        img: Zelda,
-        url: "https://ddjr.github.io/adventureGame/"
-    },
-    {
-        id: 2,
-        title: "Amazon Rebuild",
-        description: "Login. Fill your shopping cart. Review your order. Check out using stripe. Built with React, Sass, and Stripe API. Firebase for auth, database, and hosting.",
-        img: Amazon,
-        url: "https://clone-22885.firebaseapp.com/"
-    },
-    {
-        id: 3,
-        title: "Disney Plus Rebuild",
-        description: "Search for your favorite movie and view previews before watching. Built with React, Redux, and Firebase for Auth, database, and hosting.",
-        img: Disney,
-        url: "https://disney-clone-8bff4.firebaseapp.com/"
-    },
-    {
-        id: 4,
-        title: "Arcade Adventure",
-        description: "Play 6 classic arcade games, right in the browser. Built with Pure JS and OOP. Play Pong, Brick Brake, Runner, Snake, Space Invader, and Flappy Bird.",
-        img: Arcade,
-        url: "https://ddjr.github.io/arcade-games/"
-    }
+  {
+    id: 1,
+    title: "Adventure Game",
+    description:
+      "Use your arrow keys to find the grail in every level. Player learns to swim and unlock doors. Built with OOP & pure JS. Uses a Canvas element and clock to render levels. Dynamic camera scrolls as player moves.",
+    img: Zelda,
+    url: "https://ddjr.github.io/adventureGame/",
+  },
+  {
+    id: 5,
+    title: "Algorithm Visualizer",
+    description:
+      "Visualize Dijkstras algorithm.Watch as it moves through a weighted plain. Add walls to see how the algorithm finds another way. See the highlighted shortest path at the end!",
+    img: Visualizer,
+    url: "https://ddjr.github.io/pathfinding-visualizer/",
+  },
+  {
+    id: 2,
+    title: "Amazon Rebuild",
+    description:
+      "Login. Fill your shopping cart. Review your order. Check out using stripe. Built with React, Sass, and Stripe API. Firebase for auth, database, and hosting.",
+    img: Amazon,
+    url: "https://clone-22885.firebaseapp.com/",
+  },
+  {
+    id: 3,
+    title: "Disney Plus Rebuild",
+    description:
+      "Search for your favorite movie and view previews before watching. Built with React, Redux, and Firebase for Auth, database, and hosting.",
+    img: Disney,
+    url: "https://disney-clone-8bff4.firebaseapp.com/",
+  },
+  {
+    id: 4,
+    title: "Arcade Adventure",
+    description:
+      "Play 6 classic arcade games, right in the browser. Built with Pure JS and OOP. Play Pong, Brick Brake, Runner, Snake, Space Invader, and Flappy Bird.",
+    img: Arcade,
+    url: "https://ddjr.github.io/arcade-games/",
+  },
 ];
